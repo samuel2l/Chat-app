906c5c32998bdcd41a68dea9c69824fd092bac33
samuel2l <sama29571@gmail.com>
2024-03-29 19:59:58 +0000

    did register screen

---
+++ lib/chats.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    final currentUser = _auth.currentUser;
+    if (currentUser != null) {
+      print(currentUser.email);
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('chat app'),
+      ),
+      body: const Text('Hey there {currentUser.email}'),
+    );
+  }
+}
