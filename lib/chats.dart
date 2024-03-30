87fc2162885975a0496552ab490eff7a7b6f06d8
samuel2l <sama29571@gmail.com>
2024-03-30 11:24:07 +0000

    added streams to listen for new msgs

--- lib/chats.dart
+++ lib/chats.dart
@@ -39,13 +39,28 @@ class _ChatScreenState extends State<ChatScreen> {
   void getMessages() async {
     //can do it like this but how would you be getting new messages constantly? you need to use a stream so everytime new data comes you get it.
     //if you do it this way it means you would have to manually be checking the db either using a timer or smth for new data
-    final msgs = await _firestore.collection('messages').get();
+    // final msgs = await _firestore.collection('messages').get();
     //  retrieves all documents from the "messages" collection in Firestore. The get() method returns a QuerySnapshot containing the documents.
-    for (var msg in msgs.docs) {
-      // iterates over each document in the QuerySnapshot.
-      // Within the loop, msg.data() retrieves the data of the current document as a Map<String, dynamic>
-      print(msg.data()['sender']);
-      print(msg.data()['text']);
+    // for (var msg in msgs.docs) {
+    // iterates over each document in the QuerySnapshot.
+    // Within the loop, msg.data() retrieves the data of the current document as a Map<String, dynamic>
+    // print(msg.data()['sender']);
+    // print(msg.data()['text']);
+    //SO WE GOING TO USE STREAMS:
+    //a stream is like a list of futures
+    //line below subscribes to the stream
+    //toawait we use the await for keywords
+    final stream = _firestore.collection('messages').snapshots();
+    print('the stream form:');
+    print(stream);
+    await for (var snapshot in stream) {
+      //now we have looped over the snapshots of the stream which compo the same elements we looped over initally
+      //hence we can just copy the previous code
+      for (var msg in snapshot.docs) {
+        print(msg.data()['sender']);
+        print(msg.data()['text']);
+      }
+      //to get messages we will use a nested loop cos we first looping over stream then looping of the documents in each element of the stream
     }
   }
 
@@ -70,9 +85,9 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
           TextButton(
               onPressed: () {
-                // _firestore
-                //     .collection('messages')
-                //     .add({'sender': email, 'text': msg});
+                _firestore
+                    .collection('messages')
+                    .add({'sender': email, 'text': msg});
                 getMessages();
               },
               child: Text('Send'))
